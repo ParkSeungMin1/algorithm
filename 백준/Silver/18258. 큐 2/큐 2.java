ea55819146c11f3fc467e0feab7a8be36c46d029
ParkSeungMin1 <106250439+ParkSeungMin1@users.noreply.github.com>
2025-01-15 21:46:18 +0900

    [Silver IV] Title: 큐 2, Time: 1292 ms, Memory: 408440 KB -BaekjoonHub

---
+++ 백준/Silver/18258. 큐 2/큐 2.java
@@ -0,0 +1,38 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        Deque<Integer> q = new LinkedList<>();
+        int n = Integer.parseInt(br.readLine());
+
+        for(int i=1;i<=n;i++){
+            String[] s = br.readLine().split(" ");
+            if(s[0].equals("push")){
+                q.add(Integer.parseInt(s[1]));
+            }
+            else if(s[0].equals("pop")){
+                if(q.peek() == null) sb.append("-1\n");
+                else sb.append(q.poll()).append("\n");
+            }
+            else if(s[0].equals("size")){
+                sb.append(q.size()).append("\n");
+            }
+            else if(s[0].equals("empty")){
+                if(q.isEmpty()) sb.append("1\n");
+                else sb.append("0\n");
+            }
+            else if(s[0].equals("front")){
+                if(q.isEmpty()) sb.append("-1\n");
+                else sb.append(q.peek()).append("\n");
+            }
+            else {
+                if (q.isEmpty()) sb.append("-1\n");
+                else sb.append(q.peekLast()).append("\n");
+            }
+        }
+        System.out.print(sb);
+    }
+}
