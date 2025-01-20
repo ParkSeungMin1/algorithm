0f723c56efe849361fb5d0adbae22cf6216ce655
ParkSeungMin1 <106250439+ParkSeungMin1@users.noreply.github.com>
2025-01-20 16:23:31 +0900

    [level 2] Title: [PCCP 기출문제] 2번 / 퍼즐 게임 챌린지, Time: 38.59 ms, Memory: 119 MB -BaekjoonHub

---
+++ 프로그래머스/2/340212. ［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지/［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int solution(int[] diffs, int[] times, long limit) {
+        int answer = boundrySearch(diffs,times,limit);
+        return answer;
+    }
+    public static int boundrySearch(int[] diffs, int[] times, long limit){
+        int left = 1;
+        int right = Integer.MAX_VALUE-5;
+        int mid;
+        int answer=0;
+        for(;left<=right;){
+            mid = (left + right +1) / 2;
+            if(puzzle(diffs,times,limit,mid)){
+                right = mid-1;
+                answer = mid;
+            }
+            else{
+                left = mid+1;
+            }
+        }
+        return answer;
+    }
+    public static boolean puzzle(int[] diffs, int[] times, long limit,int level){
+        long totalTime=times[0];
+        for(int i=1;i<diffs.length;i++){
+            if(diffs[i]<=level) totalTime+=times[i];
+            else{
+                totalTime += (times[i-1]+times[i])*(diffs[i]-level) + times[i];
+            }
+        }
+        if(limit>=totalTime) return true;
+        else return false;
+    }
+}
